0fc8bcb7d3cfd596430c1492160c0ff01e9ab34a
Matheus de Oliveira <eumatheusdeoliveirajc@gmail.com>
2023-10-16 21:25:57 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function calcular() {
-    let n1 = Number(document.querySelector("#n1").value)
-    let n2 = Number(document.querySelector("#n2").value)
-    let resultado = document.querySelector("#resultado")
-    let operador = document.querySelector("#operadores").value
-    switch(operador) {
+    const n1 = Number(document.querySelector('#n1').value)
+    const n2 = Number(document.querySelector('#n2').value)
+    const operador = document.querySelector('#operadores').value
+    const resultado = document.querySelector('#resultado')
+    switch(operador){
         case '+':
             resultado.innerHTML = `${n1} + ${n2} = ${n1 + n2}`
             break
         case '-':
             resultado.innerHTML = `${n1} - ${n2} = ${n1 - n2}`
-            break
-        case 'x':
+        break
+        case '*':
             resultado.innerHTML = `${n1} x ${n2} = ${n1 * n2}`
             break
         case '/':
             resultado.innerHTML = `${n1} / ${n2} = ${n1 / n2}`
+            break
     }
 }
